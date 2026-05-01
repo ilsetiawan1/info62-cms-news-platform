2e68e671bf0f27439536a40a3a9c4cfd6531eb7d
Muhammad Ilham Setiawan <m.ilsetiawan1@gmail.com>
2026-05-02 01:16:57 +0700

    feat(auth): configure admin middleware aliases and custom auth redirects

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\AdminMiddleware::class,
+            'super_admin' => \App\Http\Middleware\SuperAdminMiddleware::class,
+        ]);
+
+        $middleware->redirectUsersTo('/seputaradmin/dashboard');
+        $middleware->redirectGuestsTo('/seputaradmin/login');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
